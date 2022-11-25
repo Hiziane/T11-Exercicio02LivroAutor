efe21c9504891f2ab136e8b22f7212492a1be62c
Hiziane <hizianef@gmail.com>
2022-11-25 09:55:35 -0300

    primeiro commit

---
+++ Models/Livro.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace exer02.Models
+{
+    public class Livro
+    {
+        public string? NomeLivro { get; set; }
+        public string? Autores { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using exer02.Models;
+
+Autores a = new Autores();
+a.email = "juca@gmail.com";
+Console.WriteLine($"email: {a.email}");
+Livro l =new Livro();
+
+l.NomeLivro = "Harry Potter";
+Console.WriteLine($"Livro : {l.NomeLivro}");
+List<String> listaAutores = new List<string>();
+listaAutores.Add("Juca");
+listaAutores.Add("Maria");
+listaAutores.Add("João");
+listaAutores.Add("Mari");
+listaAutores.Add("Elly");
+
+for (Int32 i=0; i<listaAutores.Count; ++i)
+{
+    Console.WriteLine("Autor: "+listaAutores[i]);
+}
+Console.WriteLine("\n");
